f64e8d4b066e973ab590556de3d44c42c946876e
Alexander Potashev <a.potashev@plazlink.com>
2014-06-14 17:00:45 +0400

    Show message "No cameras connected" when list of cameras is empty

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -42,11 +42,27 @@ void MainWindow::slotReloadCameras()
 void MainWindow::slotReloadCamerasReady(CameraInfoList result)
 {
     m_ui->camerasTableWidget->clearContents();
-    m_ui->camerasTableWidget->setRowCount(result.size());
-    for (int i = 0; i < result.size(); ++i) {
+    m_ui->camerasTableWidget->clearSpans();
+
+    int count = result.size();
+    m_ui->camerasTableWidget->setRowCount(count);
+    for (int i = 0; i < count; ++i) {
         CameraInfo cam = result.at(i);
         m_ui->camerasTableWidget->setItem(i, 0, new QTableWidgetItem(QString("%1").arg(cam.index)));
         m_ui->camerasTableWidget->setItem(i, 1, new QTableWidgetItem(cam.serialNumber));
+    }
+
+    if (result.size() == 0) {
+        // http://www.codeprogress.com/cpp/libraries/qt/showQtExample.php?index=201&key=QTableWidgetMergeCells
+        m_ui->camerasTableWidget->setRowCount(1);
+
+        QTableWidgetItem* item = new QTableWidgetItem("No cameras connected");
+        item->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+        QFont font = item->font();
+        font.setItalic(true);
+        item->setFont(font);
+        m_ui->camerasTableWidget->setItem(0, 0, item);
+        m_ui->camerasTableWidget->setSpan(0, 0, 1, 2);
     }
 }
 
